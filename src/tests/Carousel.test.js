2c0f474a19c0050a6ddef1e05979e9867a99056b
Ashraful Islam <rx.ju.edu@gmail.com>
2021-07-01 19:39:07 +0700

    :sparkles: Allow the index in HasIndex to be controlled

--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Carousel, { Carousel as CoreCarousel } from "../Carousel";
 import CarouselButton from "../CarouselButton";
 import CarouselSlide from "../CarouselSlide";
@@ -88,6 +88,13 @@ describe("Carousel", () => {
 
     it("should pass `slides` to the core component", () => {
       expect(wrapper.find(CoreCarousel).prop("slides")).toBe(slides);
+    });
+
+    it("allows `slideIndex` to be controlled", () => {
+      const mounted = mount(<Carousel slides={slides} slideIndex={1} />);
+      expect(mounted.find(CoreCarousel).prop("slideIndex")).toBe(1);
+      mounted.setProps({ slideIndex: 0 });
+      expect(mounted.find(CoreCarousel).prop("slideIndex")).toBe(0);
     });
   });
 
--- src/tests/HasIndex.test.js
+++ src/tests/HasIndex.test.js
@@ -43,4 +43,38 @@ describe("HasIndex()", () => {
     wrapper.prop("indexIncrement")(3);
     expect(wrapper.state("index")).toBe(0);
   });
+
+  it("should have index:0 for uncontrolled and index:1 for controller", () => {
+    expect(wrapper.state("index")).toBe(0);
+    const wrapper2 = shallow(<MockComponentWithIndex defaultIndex={1} />);
+    expect(wrapper2.state("index")).toBe(1);
+  });
+
+  it("always has `index` state equal to the `index` prop", () => {
+    const wrapperWithInitialIndex = shallow(
+      <MockComponentWithIndex index={1} />
+    );
+    expect(wrapperWithInitialIndex.state("index")).toBe(1);
+    wrapper.setProps({ index: 2 });
+    expect(wrapper.state("index")).toBe(2);
+  });
+
+  // Uncontrolled
+  it("allows `index` state to change if the `index` prop is unset", () => {
+    const wrapperWithInitialIndex = shallow(
+      <MockComponentWithIndex index={1} />
+    );
+    wrapperWithInitialIndex.setProps({ index: undefined });
+    wrapperWithInitialIndex.setState({ index: 3 });
+    expect(wrapperWithInitialIndex.state("index")).toBe(3);
+  });
+
+  it("calls `onIndexChange` on decrement/increment", () => {
+    const onIndexChange = jest.fn();
+    wrapper.setProps({ index: 0, onIndexChange });
+    wrapper.prop("indexDecrement")(3);
+    expect(onIndexChange).toHaveBeenCalledWith({ target: { value: 2 } });
+    wrapper.prop("indexIncrement")(3);
+    expect(onIndexChange).toHaveBeenCalledWith({ target: { value: 1 } });
+  });
 });
